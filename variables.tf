ba81b919753753ced0c1c1e97acdfe3ad4a12a4f
Marcel Graf <grafolytics@gmail.com>
2021-05-07 19:28:24 +0200

    fix: configure cluster repository and project

--- variables.tf
+++ variables.tf
@@ -3,7 +3,8 @@
 // ----------------------------------------------------------------------------
 variable "region" {
   description = "AWS region code for creating resources."
-  type = string
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "cluster_version" {
@@ -21,29 +22,31 @@ variable "vault_user" {
 variable "cluster_name" {
   description = "Name of the Kubernetes cluster to create"
   type        = string
-  default     = ""
+  default     = "JenkinsXTest"
 }
 
 variable "force_destroy" {
   description = "Flag to determine whether storage buckets get forcefully destroyed. If set to false, empty the bucket first in the aws s3 console, else terraform destroy will fail with BucketNotEmpty error"
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "is_jx2" {
-  default     = false
   type        = bool
   description = "Flag to specify if jx2 related resources need to be created"
+  default     = false
 }
 
 variable "jx_git_url" {
   description = "URL for the Jenins X cluster git repository"
   type        = string
+  default     = "https://github.com/gitops-playground/jx3-eks-vault"
 }
 
 variable "jx_bot_username" {
   description = "Bot username used to interact with the Jenkins X cluster git repository"
   type        = string
+  default     = "grafolytics-bot"
 }
 
 variable "jx_bot_token" {
